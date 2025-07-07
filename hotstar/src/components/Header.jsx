2d9e011c8fb7a15fc0a2e437c256d44e5b5315b6
Ankit Talukder <codingjourney18@gmail.com>
2025-07-07 12:05:18 +0530

    Masde the header part complete ig

--- hotstar/src/components/Header.jsx
+++ hotstar/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     HiHome,
     HiMagnifyingGlass,
@@ -13,6 +13,8 @@ import HeaderItems from './HeaderItems';
 
 
 function Header() {
+
+    const [toggle,setToggle]=useState(false);
     const menu = [
         {
             name: 'HOME',
@@ -41,17 +43,46 @@ function Header() {
     ];
 
     return (
+    
         <div className='flex items-center justify-between p-5'>
             <div className='flex gap-8'>
-                <img src='/images/Logo.png' className='w-[80px] md:w-[115px] object-cover p-2 mr-25 ml-10'></img>
-                {menu.map((item) =>
-                    <li key={item.name} className='mt-2 mr-3'>
-                        <HeaderItems name={item.name} Icons={item.icon} />
-                        {/* //Using props property */}
-                    </li>
-                )}
+
+                <img src='/images/Logo.png' className='w-[80px] md:w-[115px] object-cover p-5'></img>
+
+                <div className='hidden md:flex gap-8'>
+                    {menu.map((item) =>
+                        <li key={item.name} className='mt-2 mr-3'>
+                            <HeaderItems name={item.name} Icons={item.icon} />
+                            {/* //Using props property */}
+                        </li>
+                    )}
+                </div>
+
+                <div className='flex md:hidden gap-8'>
+                    {menu.map((item, index) => index < 3 && (
+                        <li key={item.name} className='mr-3'>
+                            <HeaderItems name={''} Icons={item.icon} />
+                            {/* //Using props property */}
+                        </li>
+                    ))}
+                    <div className=' md:hidden mt-6' onClick={()=>setToggle(!toggle)}>
+                        <HeaderItems name={''} Icons={HiDotsVertical} />
+                        {toggle?<div className='absolute mt-3 bg-[#121212]
+                         border-[1px] border-gray-700 p-3 px-5 py-4
+                        '>
+                            {menu.map((item, index) => index > 2 && (
+
+                                <li key={item.name} className='mr-3'>
+                                    <HeaderItems name={item.name} Icons={item.icon} />
+                                    {/* //Using props property */}
+                                </li>
+                            ))}
+                        </div>:null}
+                    </div>
+                </div>
+
             </div>
-            <img src='/images/pp.jpg' alt='profile-photo' className='w-[60px] rounded-full'/>
+            <img src='/images/pp.jpg' alt='profile-photo' className='w-[60px] rounded-full mr-[15px]' />
         </div>
 
     );
--- hotstar/src/components/HeaderItems.jsx
+++ hotstar/src/components/HeaderItems.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function HeaderItems({name,Icons}) {
   return (
     <div className='text-white flex items-center gap-3 text-[25px]
-     font-semibold cursor-pointer hover:underline underline-offset-8'   >
+     font-semibold cursor-pointer hover:underline underline-offset-8 mb-2'   >
         <Icons/>
-        <h2>{name}</h2>
+        <h2 className=''>{name}</h2>
     </div>
   )
 }
