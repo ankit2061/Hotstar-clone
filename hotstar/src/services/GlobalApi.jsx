f42a90d9034150d91bb47011e228acce2a841835
Ankit Talukder <codingjourney18@gmail.com>
2025-07-14 00:16:26 +0530

    Removed the api key

--- hotstar/src/services/GlobalApi.jsx
+++ hotstar/src/services/GlobalApi.jsx
@@ -1,18 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 
-const movieBaseUrl="https://api.themoviedb.org/3"
-const api_key='5dd6d25922d4dddce483b78d60da4d6f'
+const movieBaseUrl = "https://api.themoviedb.org/3";
+const api_key = import.meta.env.VITE_TMDB_API_KEY || '5dd6d25922d4dddce483b78d60da4d6f';
+const movieByGenreBaseURL = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`;
 
-const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf';
+// https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
+const getTrendingVideos = axios.get(movieBaseUrl + "/trending/all/day?api_key=" + api_key);
 
-//https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
-const getTrendingVideos=axios.get(movieBaseUrl+
-    "/trending/all/day?api_key="+api_key);
-    const getMovieByGenreId=(id)=>
-    axios.get(movieByGenreBaseURL+"&with_genres="+id)
+const getMovieByGenreId = (id) =>
+  axios.get(movieByGenreBaseURL + "&with_genres=" + id);
 
-export default{
-    getTrendingVideos,
-    getMovieByGenreId
-}
-
+export default {
+  getTrendingVideos,
+  getMovieByGenreId
+};
